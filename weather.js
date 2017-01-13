9085d864becb9b9fa81ed5269560838b99099561
Stephanie Chin <stephjuechin@gmail.com>
2017-01-12 23:20:55 -0800

    Created function to change background image and font colors for night vs day

--- weather.js
+++ weather.js
@@ -2,34 +2,50 @@ $(document).ready(function() {
   // console.log("ready!");
   currentTime();
 
-
   //function to add time icons
   function currentTime() {
     //get current time
     var timeNow = new Date(Date.now());
     console.log(timeNow);
     //creates substring of numbers before first :
-    hour = timeNow.toLocaleTimeString().substring(0, timeNow.toLocaleTimeString().indexOf(":"));
+    hour = timeNow.toLocaleTimeString().substring(0, timeNow.toLocaleTimeString().indexOf(':'));
     console.log(hour);
     //change icon class to new icon using getTimeIcon function
     $('#time_icon').removeClass().addClass(getTimeIcon(hour));
+    changeBackgroundImg(timeNow);
+  }
+
+  function changeBackgroundImg(time) {
+    time = time.toString();
+    console.log(time);
+    hour = (time.substring(time.indexOf(':') - 2, time.indexOf(':')));
+    console.log(hour);
+    if (hour >= 17 || hour <= 5) {
+      $('body').css({'background': "url('https://static.pexels.com/photos/26404/pexels-photo.jpg') no-repeat center center fixed"});
+      $('body').css({"background-size":"cover"});
+      $('body').css({"color":"#c4e5cf"});
+    } else {
+      $('body').css('background', "url('https://static.pexels.com/photos/128994/pexels-photo-128994.jpeg' no-repeat center center fixed");
+      $('body').css({"background-size":"cover"});
+      $('body').css({"color":"#3e2887"});
+    }
   }
 
   function getTimeIcon(hour) {
     //object of time icon class key pairs
     var time_icons = {
-      "1": "wi wi-time-1",
-      "2": "wi wi-time-2",
-      "3": "wi wi-time-3",
-      "4": "wi wi-time-4",
-      "5": "wi wi-time-5",
-      "6": "wi wi-time-6",
-      "7": "wi wi-time-7",
-      "8": "wi wi-time-8",
-      "9": "wi wi-time-9",
-      "10": "wi wi-time-10",
-      "11": "wi wi-time-11",
-      "12": "wi wi-time-12"
+      '1': 'wi wi-time-1',
+      '2': 'wi wi-time-2',
+      '3': 'wi wi-time-3',
+      '4': 'wi wi-time-4',
+      '5': 'wi wi-time-5',
+      '6': 'wi wi-time-6',
+      '7': 'wi wi-time-7',
+      '8': 'wi wi-time-8',
+      '9': 'wi wi-time-9',
+      '10': 'wi wi-time-10',
+      '11': 'wi wi-time-11',
+      '12': 'wi wi-time-12'
     };
    return time_icons[hour];
    }
@@ -45,6 +61,7 @@ $(document).ready(function() {
     console.log('Your nav.geo current position is:');
     console.log(`Latitude: ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
+
     //api call to openweather map with lat/lng coordinates
     $.ajax( {
       url: "http://api.openweathermap.org/data/2.5/weather?lat=" + crd.latitude + "&lon=" + crd.longitude + "&APPID=31f8be6186325550e955c4ee2566a726",
@@ -104,6 +121,12 @@ $(document).ready(function() {
   function getWeatherInfo(data) {
     var weather = data;
     console.log(weather);
+    //changes icon class to current weather icon via getIcon
+    $('#icon').attr('class', getWeatherIcon(weather.weather[0].icon));
+    console.log(weather.weather)
+    console.log(weather.weather[0].main);
+    console.log(weather.weather[0].description);
+    console.log(weather.weather[0].icon);
     //city, country
     $('#location').html(weather.name + ", " + weather.sys.country);
     //converts sunrise into locale time string
@@ -121,35 +144,29 @@ $(document).ready(function() {
     $('#weather_main').html(weather.weather[0].main);
     $('#weather_desc').html(weather.weather[0].description);
     $('#weather_icon').html(weather.weather[0].icon);
-    //changes icon class to current weather icon via getIcon
-    $('#icon').removeClass().addClass(getWeatherIcon(weather.weather[0].icon));
-    console.log(weather.weather)
-    console.log(weather.weather[0].main);
-    console.log(weather.weather[0].description);
-    console.log(weather.weather[0].icon);
   }
 
   function getWeatherIcon(icon) {
     // console.log(icon);
     var icon_list = {
-      "01d": "wi wi-day-sunny",
-      "02d": "wi wi-day-cloudy",
-      "03d": "wi wi-day-sunny-overcast",
-      "04d": "wi wi-day-cloudy-high",
-      "09d": "wi wi-day-showers",
-      "10d": "wi wi-day-rain",
-      "11d": "wi wi-day-thunderstorm",
-      "13d": "wi wi-day-snow",
-      "50d": "wi wi-day-fog",
-      "01n": "wi wi-night-clear",
-      "02n": "wi wi-night-cloudy",
-      "03n": "wi wi-night-alt-partly-cloudy",
-      "04n": "wi wi-night-cloudy-high",
-      "09n": "wi wi-night-showers",
-      "10n": "wi wi-night-rain",
-      "11n": "wi wi-night-thunderstorm",
-      "13n": "wi wi-night-snow",
-      "50n": "wi wi-night-fog"
+      '01d': 'wi wi-day-sunny',
+      '02d': 'wi wi-day-cloudy',
+      '03d': 'wi wi-day-sunny-overcast',
+      '04d': 'wi wi-day-cloudy-high',
+      '09d': 'wi wi-day-showers',
+      '10d': 'wi wi-day-rain',
+      '11d': 'wi wi-day-thunderstorm',
+      '13d': 'wi wi-day-snow',
+      '50d': 'wi wi-day-fog',
+      '01n': 'wi wi-night-clear',
+      '02n': 'wi wi-night-cloudy',
+      '03n': 'wi wi-night-alt-partly-cloudy',
+      '04n': 'wi wi-night-cloudy-high',
+      '09n': 'wi wi-night-showers',
+      '10n': 'wi wi-night-rain',
+      '11n': 'wi wi-night-thunderstorm',
+      '13n': 'wi wi-night-snow',
+      '50n': 'wi wi-night-fog'
       };
     // if icon_list isn't found  return N/A
     if (icon_list[icon] === undefined) {
